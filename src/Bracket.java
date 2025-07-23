786b116aa4965a5f0d2f1ac2525eba21b019d3a1
SusKing123 <maqeelakbari@gmail.com>
2025-07-22 20:52:26 -0500

    quick code
    Added functions to bracket
    - removePlayer()
    - bracketName()
    - bracketSorter()

--- src/Bracket.java
+++ src/Bracket.java
@@ -8,11 +8,40 @@ public class Bracket {
         this.name = bName;
     }
 
+    // Adds a player to the bracket
     public boolean addPlayer(String playerName)
     {
         Player newPlayer = new Player(playerName);
         players.add(newPlayer);
         return true;
+    }
+
+    // Removes player from the bracket
+    public boolean removePlayer(String playerName)
+    {
+        for(int i = 0; i < players.size(); i++)
+        {
+            if(players.get(i).getName() == playerName)
+            {
+                players.remove(i);
+                return true;
+            }
+        }
+        System.out.println("Player does not exist. ");
+        return false;
+    }
+
+    // Changes the name of the bracket
+    public void bracketName(String newName)
+    {
+        this.name = newName;
+    }
+
+    // Sorts the bracket
+    //TODO
+    private void bracketSorter()
+    {
+        ArrayList<Player> temp = players;
     }
 
     public static void main(String[] args) {
