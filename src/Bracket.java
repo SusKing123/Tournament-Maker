6325e4df8971d71dc56737216fa0a9f600b4da86
SusKing123 <maqeelakbari@gmail.com>
2025-07-24 19:39:24 -0500

    Started on mergeSort
    started on the mergeSort method to sort the bracket by rank

--- src/Bracket.java
+++ src/Bracket.java
@@ -44,6 +44,68 @@ public class Bracket {
         ArrayList<Player> temp = players;
     }
 
+    private static void mergeSort(int[] array)
+    {
+        int length = array.length;
+        if (length <= 1) return;
+
+        int middle = length / 2;
+        int[] left = new int[middle];
+        int[] right = new int[length - middle];
+
+        int i = 0;
+        int j = 0;
+
+        for(; i < length; i++)
+        {
+            if(i < middle) {
+                left[i] = array[i];
+            }
+            else {
+                right[j] = array[i];
+                j++;
+            }
+        }
+        mergeSort(left);
+        mergeSort(right);
+        merge(left,right,array);
+    }
+
+    private static void merge(int[] left, int[] right, int[] array)
+    {
+        int leftSize = array.length / 2;
+        int rightSize = array.length - leftSize;
+        int i = 0, l = 0, r = 0;
+
+        while(l < leftSize && r < rightSize)
+        {
+            if(left[l] < right[r])
+            {
+                array[i] = left[l];
+                i++;
+                l++;
+            }
+            else
+            {
+                array[i] = right[r];
+                i++;
+                r++;
+            }
+        }
+        while(l < leftSize)
+        {
+            array[i] = left[l];
+            i++;
+            l++;
+        }
+        while(r < rightSize)
+        {
+            array[i] = right[r];
+            i++;
+            r++;
+        }
+    }
+
     public static void main(String[] args) {
         Bracket test = new Bracket("test");
         
